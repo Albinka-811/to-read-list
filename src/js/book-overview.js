ffcf71cccff8a6f9d210f86d7debab620a84b384
Albina Holovach <albinkasolodenko@gmail.com>
2021-04-25 13:57:59 +0300

    Create template function for book overview class

--- src/js/book-overview.js
+++ src/js/book-overview.js
@@ -1,31 +1,32 @@
 export class BookOverview {
     container = null;
+    EMPTY_DATA_PLACEHOLDER = 'NO DATA';
 
     constructor (container) {
         this.container = document.getElementById(container);
     }
 
+    createDescriptionRaw (title, value) {
+        return `
+            <div class="book-details-body-text">
+                ${ title }
+                <p class="book-details-body-text__value">${ value ?? this.EMPTY_DATA_PLACEHOLDER }</p>
+            </div>
+        `;
+    }
+
     render (bookInfo) {
-        const PLACEHOLDER = 'NO DATA';
 
         this.container.innerHTML = `
-               <h2 class="book-details-title">${ bookInfo.title }</h2>
-               <h3 class="book-details-author">${ bookInfo.author_name?.join(' ') ?? PLACEHOLDER }</h3>
+           <h2 class="book-details-title">${ bookInfo.title }</h2> 
+           <h3 class="book-details-author">${ bookInfo.author_name?.join(' ') ?? this.EMPTY_DATA_PLACEHOLDER }</h3>
             
             <div class="book-details-body">
-                <div class="book-details-body-text">Languages available: ${ bookInfo?.language.join(', ') ?? PLACEHOLDER }</div>
-                <div class="book-details-body-text">
-                    First publish year: 
-                    <p class="book-details-body-text__value">${ bookInfo.publish_year?.[0] ?? PLACEHOLDER }</p>
-                </div>
-                <div class="book-details-body-text">
-                    Years published: 
-                    <p class="book-details-body-text__value">${ bookInfo.publish_year?.join(', ') }</p>
-                </div>
-                <div class="book-details-body-text">
-                    Full text available: 
-                    <p class="book-details-body-text__value">${ bookInfo.text }</p>
-                </div>
+                <div class="book-details-body-text">Languages available: ${ bookInfo.language?.join(', ') ?? this.EMPTY_DATA_PLACEHOLDER }</div>
+                
+                ${ this.createDescriptionRaw('First publish year:', bookInfo.publish_year?.[0]) }
+                ${ this.createDescriptionRaw('Years published:', bookInfo.publish_year?.join(', ')) }
+                ${ this.createDescriptionRaw('Full text available:', bookInfo.text) }
             </div>
         `;
     }
